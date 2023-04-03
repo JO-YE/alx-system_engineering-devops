b7e434e94f75adc1eecf61c73fd2aba5c6becf43
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-03 21:26:51 +0100

    puppet code for task 0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,5 @@
 # Installs a Nginx server with custome HTTP header
 
-package {'nginx':
-    ensure => 'present',
-}
-
 exec {'install'
     command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
     provider => shell,
@@ -11,11 +7,11 @@ exec {'install'
 
 exec {'add_header':
     provider => shell,
-    command => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
     environment => ["HOST=${hostname}"],
+    command => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
 }
 
-exec {'run':
+exec {'restart':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
