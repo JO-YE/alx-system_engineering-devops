991d8f3ebc4e4dcca0c308253411c7a0d90db192
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-05-18 13:49:14 +0100

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # finding out why an apache is returning 500 error using tmux and strace
 
 file { '/var/www/html/wp-settings.php':
-    ensure => present,
+    ensure  => present,
     content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
 }
